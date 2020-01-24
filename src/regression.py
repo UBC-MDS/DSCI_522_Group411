bf66ba9c55368fa277a7aa7080d1aaa508c382d4
Katie Birchard <birchardkatie@gmail.com>
2020-01-24 12:54:20 -0800

    created usage for python script, deleted extra analysis

---
+++ src/regression.py
@@ -0,0 +1,12 @@
+# author: Katie Birchard
+# date: 2020-01-24
+
+"""This script conducts the regression analysis.
+
+Usage: regression.py --input=<input> --output=<output>
+
+Options:
+--input=<input>   Path to a cleaned dataset in feather form
+--output=<output> Path to the figures/tables output by the regression analysis
+"""
+
